d05d4a227946e59cca52e3e5a5855e579a56ab53
Thành Trung <61226153+trungdongphuoca@users.noreply.github.com>
2024-05-23 11:41:02 +0700

    Update Conduit and Support page

--- app.py
+++ app.py
@@ -15,5 +15,13 @@ def cable_calculate():
 def cable_tray():
     return render_template('cable-tray-page.html')
 
+@app.route('/conduit')
+def conduit():
+    return render_template('conduit-page.html')
+
+@app.route('/support')
+def support():
+    return render_template('support-page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
